20c3c05c684c5207875b6086049ed74448465bb0
peterkomo <mwangipeterkomo@gmail.com>
2023-08-17 15:40:23 +0300

    used list  that returns alist off all even numbers

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    return [num for num in num_list if num % 2 == 0]
+    results = return_evens([0, 1, 3, 5, 7, 8, 9])
+    print(results)
+
+  
     pass
 
 def make_exclamation(sentence_list):
